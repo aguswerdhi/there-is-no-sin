ce9dd0fd35ae3719d991cea6782c5d159018c63f
aguswerdhi <152775505+aguswerdhi@users.noreply.github.com>
2025-05-01 15:34:31 +0800

    Create game.py

---
+++ game.py
@@ -0,0 +1,16 @@
+from engine.story_loader import load_story
+from engine.game_logic import run_game
+from engine.endings import show_ending
+from time import sleep
+
+def display_intro():
+    with open("assets/intro.txt", "r") as f:
+        for line in f:
+            print(line.strip())
+            sleep(1.5)
+
+if __name__ == "__main__":
+    display_intro()
+    story = load_story("story/main_story.json")
+    karma = run_game(story)
+    show_ending(karma)
